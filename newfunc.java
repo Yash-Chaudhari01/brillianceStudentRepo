81964f2040f9f5205d459ba83474c527391e7619
DARSHAN <darshandesale143@gmail.com>
2025-06-07 20:28:08 +0530

    payment gateway done

--- newfunc.java
+++ newfunc.java
@@ -1,6 +1,5 @@
 public class newfunc {
     public static void main(String[] args) {
         System.out.println("payment gateway done");
-        System.out.println("New payment system is been created");
     }
 }
